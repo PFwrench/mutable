1351f97c61ea8e6c863e2f08360eb238f094bb87
Alex Arze <alexarze@gmail.com>
2016-10-01 15:05:42 -0500

    Revised to use twitter client library for node.js

--- lambda/PostMute.js
+++ lambda/PostMute.js
@@ -1,8 +1,14 @@
-// Twitter Credentials
-var consumerKey = 'LJED4VTgNSd8WQ0S5bAmi5pZB';
-var consumerSecret = 'wye1TtPPmoGMCebWVZRK8YyM7f0g88rHmRX2uxYcsDS5YMBHrY';
-
 var https = require('https');
+var Twitter = require('twitter');
+
+var client = new Twitter({
+    consumer_key: 'LJED4VTgNSd8WQ0S5bAmi5pZB',
+    consumer_secret: 'wye1TtPPmoGMCebWVZRK8YyM7f0g88rHmRX2uxYcsDS5YMBHrY',
+    access_token_key: '782274256671350784-l0dWnhmZwVugjGv5gPCumPqODmQuVTt',
+    access_token_secret: 'tPs4ggAT3JqTYwM5LxFHzkcTYTe5lSVFsDZ3e0IF4oS5i'
+})
+
+var params = {user_id: ''};
 
 // Lambda function:
 exports.handler = function(event, context) {
@@ -11,44 +17,48 @@ exports.handler = function(event, context) {
 
     // Mute user ID provided in event data.
     // End the lambda function when the send function completes.
-    PostMute(event.user_id, function (status) { context.done(null, status); });
+    client.post('mutes/users/create', params, function(error) {
+        if (!error) {
+            console.log('success!');
+        }
+    });
 
 };
 
-function PostMute(target_id) {
+// function PostMute(target_id) {
 
-    // Options and headers for the HTTP requestvar 
-    options = {
-        host: 'api.twitter.com',
-        path: '/mutes/users/create.json',
-        method: 'POST',
-        headers: {
-            'user_id' : target_id
-        }
-    };
+//     // Options and headers for the HTTP requestvar 
+//     options = {
+//         host: 'api.twitter.com',
+//         path: '/mutes/users/create.json',
+//         method: 'POST',
+//         headers: {
+//             'user_id' : target_id
+//         }
+//     };
 
-    // Setup the HTTP request
-    var req = https.request(options, function (res) {
+//     // Setup the HTTP request
+//     var req = https.request(options, function (res) {
 
-        res.setEncoding('utf-8');
+//         res.setEncoding('utf-8');
 
-        // Collect response data as it comes back.
-        var responseString = '';
-        res.on('data', function (data) {
-            responseString += data;
-        });
+//         // Collect response data as it comes back.
+//         var responseString = '';
+//         res.on('data', function (data) {
+//             responseString += data;
+//         });
 
-        // Handler for HTTP request errors.
-        req.on('error', function (e) {
-            console.error('HTTP error: ' + e.message);
-            completedCallback('API request completed with error(s).');
-        });
+//         // Handler for HTTP request errors.
+//         req.on('error', function (e) {
+//             console.error('HTTP error: ' + e.message);
+//             completedCallback('API request completed with error(s).');
+//         });
 
-        // Send the HTTP request to the Twitter API.
-        // Log the message we are sending to Twitter.
-        console.log('Twitter API call: Mute user_id=' + target_id);
-        req.write(messageString);
-        req.end();
-    })
+//         // Send the HTTP request to the Twitter API.
+//         // Log the message we are sending to Twitter.
+//         console.log('Twitter API call: Mute user_id=' + target_id);
+//         req.write(messageString);
+//         req.end();
+//     })
 
-}
+// }
